aa8fca90e6c19a967ac84ef097de0047659577c5
Jaden CH <jadenjohn.ch@gmail.com>
2022-02-15 15:02:48 +0000

    Fixed not showing all top words bug

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ for word in words:
         positions[pos][letter] += 1
         pos += 1
 
+ 
 # Score words
 wordscores = {}
 for word in words:
@@ -29,8 +30,11 @@ for word in words:
         wordscores[word] += positions[pos][letter]
         pos += 1
 
+
 # Sort by highest word scores
 wordscores = list(sorted(wordscores.items(), key=lambda item: item[1]))
+
+wordscores_all = wordscores
 
 mostcommon = list(open("mostcommon5.txt", "r"))
 for i in range(len(mostcommon)):
@@ -38,45 +42,54 @@ for i in range(len(mostcommon)):
 
 def displaywords():
     # Print out top 20 words
-    top = wordscores[len(wordscores)-20:]
-    top.sort(key=lambda word: mostcommon.index(word[0]) if mostcommon.count(word[0]) > 0 else 99999999999)
+    top = wordscores[len(wordscores)-20 if len(wordscores) > 20 else 0::]
+    for word in top:
+        if mostcommon.count(word[0]) < 1:
+            mostcommon.append(word[0])
+    top.sort(key=lambda word: mostcommon.index(word[0]))
     for i in range(len(top)-1, -1, -1):
             print(str(i+1) + ": " + top[i][0] + " at score " + str(top[i][1]))
 
-# Take input
 while True:
-    displaywords()
-    wordled = input("Wordle'd: ")
-    wordmask = input("Worked: ")
-    letteroccurs = {}
-    wordstripped = wordled
-    rl = 0
-    for i in range(len(wordled)):
-        if wordmask[i] == "0":
-            wordstripped = wordstripped[:i-rl] + wordstripped[i-rl+1:]
-            rl += 1
-    removed_words = 0
-    for word in range(len(wordscores)):
-        for mask in range(len(wordmask)):
-            letteroccurs[letter] = 1
-            checkword = wordscores[word - removed_words][0]
-            if wordmask[mask] == "0":
-                if checkword[mask] == wordled[mask]:
-                    del(wordscores[word - removed_words])
-                    removed_words += 1
-                    break
-                elif checkword.count(wordled[mask]) > wordstripped.count(wordled[mask]):
-                    del(wordscores[word - removed_words])
-                    removed_words += 1
-                    break
-                    
-            elif wordmask[mask] == "1":
-                if checkword[mask] == wordled[mask] or checkword.count(wordled[mask]) < wordstripped.count(wordled[mask]):
-                    del(wordscores[word - removed_words])
-                    removed_words += 1
-                    break
-            elif wordmask[mask] == "2":
-                if checkword[mask] != wordled[mask]:
-                    del(wordscores[word - removed_words])
-                    removed_words += 1
-                    break
+    wordscores = wordscores_all.copy()
+    while True:
+        displaywords()
+        wordled = input("Wordle'd: ")
+        wordmask = input("Worked: ")
+        if wordled.lower() == "!redo" or wordmask.lower() == "!redo":
+            continue
+        elif wordled.lower() == "!restart" or wordmask.lower() == "!restart":
+            break
+        else:
+            letteroccurs = {}
+            wordstripped = wordled
+            rl = 0
+            for i in range(len(wordled)):
+                if wordmask[i] == "0":
+                    wordstripped = wordstripped[:i-rl] + wordstripped[i-rl+1:]
+                    rl += 1
+            removed_words = 0
+            for word in range(len(wordscores)):
+                for mask in range(len(wordmask)):
+                    letteroccurs[letter] = 1
+                    checkword = wordscores[word - removed_words][0]
+                    if wordmask[mask] == "0":
+                        if checkword[mask] == wordled[mask]:
+                            del(wordscores[word - removed_words])
+                            removed_words += 1
+                            break
+                        elif checkword.count(wordled[mask]) > wordstripped.count(wordled[mask]):
+                            del(wordscores[word - removed_words])
+                            removed_words += 1
+                            break
+                            
+                    elif wordmask[mask] == "1":
+                        if checkword[mask] == wordled[mask] or checkword.count(wordled[mask]) < wordstripped.count(wordled[mask]):
+                            del(wordscores[word - removed_words])
+                            removed_words += 1
+                            break
+                    elif wordmask[mask] == "2":
+                        if checkword[mask] != wordled[mask]:
+                            del(wordscores[word - removed_words])
+                            removed_words += 1
+                            break
